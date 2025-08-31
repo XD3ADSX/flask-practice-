5f868cf6fadd5ca204860924b7f9b551014c525b
mani elahi <manielahi41@gmail.com>
2025-08-31 12:38:27 +0100

    fixed css issues with login page and added register page

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def home():
 def login(): 
     return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 
 if __name__ == "__main__": 
     app.run(host='0.0.0.0', port=5555)
